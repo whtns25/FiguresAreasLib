03c365d1fe24078fcf2a7f5243d44ef17dd0c747
Андрей <cerber752mail.ru>
2020-02-25 07:55:17 +0300

    first commit

---
+++ FiguresAreasLibTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("FiguresAreasLibTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("FiguresAreasLibTests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("6c5420f4-e777-4c7f-9e98-c0aef6dafe09")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ FiguresAreasLibTests/UnitTest1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FiguresAreasLib.Tests
+{
+    [TestClass]
+    public class FiguresAreasTests
+    {
+        [TestMethod]
+        public void CirclesArea_5_and_3in__47_123returned()  //круг
+        {
+            //arrange 
+            double R = 5;
+            double r = 3;
+            double expected = 47.124;
+
+            //act
+            FiguresAreas a = new FiguresAreas();
+            double actual = a.CirclesArea(R, r);
+
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+
+
+        [TestMethod]
+        public void CirclesArea_5in__3in()    //эллипс
+        {
+            //arrange 
+            double R = 5;
+            double expected = 78.54;
+
+            //act
+            FiguresAreas a = new FiguresAreas();
+            double actual = a.CirclesArea(R);
+
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+
+
+        [TestMethod]
+        public void TrianglesArea_5_6_7in__14_7returned()   //треугольник
+        {
+            //arrange 
+            double a = 5;
+            double b = 6;
+            double c = 7;
+            double expected = 14.697;
+
+            //act
+            FiguresAreas d = new FiguresAreas();
+            double actual = d.TrianglesArea(a, b, c);
+
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+
+
+        [TestMethod]
+        public void TrianglesCheck_trueReturned()  //прямоугольный треугольник (чек)
+        {
+
+            //arrange 
+            double a = 5;
+            double b = 3;
+            double c = 4;
+            bool expected = true;
+
+            //act
+            FiguresAreas d = new FiguresAreas();
+            bool actual = d.TrianglesCheck(a, b, c);
+
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+
+
+        [TestMethod]
+        public void TrianglesCheck_falseReturned() //непрямоугольный треугольник (чек)
+        {
+            //arrange 
+            double a = 6;
+            double b = 6;
+            double c = 6;
+            bool expected = false;
+
+            //act
+            FiguresAreas d = new FiguresAreas();
+            bool actual = d.TrianglesCheck(a, b, c);
+
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+
+
+        [TestMethod]
+        public void CustomFiguresArea_8coords_16returned() //кастомная фигура, квадрат
+        {
+            //arrange 
+            const int a = 8;
+            int[] coords = new int[a] { 0, 0, 0, 4, 4, 4, 4, 0 };
+            double expected = 16;
+
+            //act
+            FiguresAreas d = new FiguresAreas();
+            double actual = d.CustomFiguresArea(coords);
+
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+
+
+        [TestMethod]
+        public void CustomFiguresArea_14coords_39returned() //кастомная фигура, произвольный семиугольник
+        {
+            //arrange 
+            const int a = 14;
+            int[] coords = new int[a] { -5, -2, -5, 2, -2, 5, 1, 5, 2, 2, 0, -1, -3, -3 };
+            double expected = 39;
+
+            //act
+            FiguresAreas d = new FiguresAreas();
+            double actual = d.CustomFiguresArea(coords);
+
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+
+    }
+}
